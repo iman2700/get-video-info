fe2b27118991e969c74aeb60bc7295d0c74da3a5
alireza5a <saeedi.mail@gmail.com>
2023-11-01 18:11:47 +0330

    add MediatR package and BaseEvent

---
+++ src/Domain/Common/BaseEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Domain.Common;
+
+// The BaseEvent class is a foundational component for handling events in the domain.
+// It uses the MediatR library for event-driven communication and implements the INotification interface.
+// This abstract class serves as a base for creating specific event types, promoting structured event handling within the domain layer.
+public abstract class BaseEvent : INotification
+{
+}
