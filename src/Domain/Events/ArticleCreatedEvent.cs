5a2614f159496dc2b20a17527faac7702e7a085b
alireza5a <saeedi.mail@gmail.com>
2023-11-02 18:10:44 +0330

    add Domain Events

---
+++ src/Domain/Events/ArticleCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Events;
+public class ArticleCreatedEvent : BaseEvent
+{
+    public ArticleCreatedEvent(Article item)
+    {
+        Item = item;
+    }
+
+    public Article Item { get; }
+}
---
+++ src/Domain/Events/ArticleDeletedEvent.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Events;
+public class ArticleDeletedEvent : BaseEvent
+{
+    public ArticleDeletedEvent(Article item)
+    {
+        Item = item;
+    }
+
+    public Article Item { get; }
+}
