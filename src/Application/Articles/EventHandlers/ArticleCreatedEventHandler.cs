e691f29a513fd3e8a57b349ff70fc432430dd1a0
alireza5a <saeedi.mail@gmail.com>
2023-11-03 13:50:42 +0330

    add EventHandlers

---
+++ src/Application/Articles/EventHandlers/ArticleCreatedEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Events;
+
+namespace Application.Articles.EventHandlers;
+internal class ArticleCreatedEventHandler : INotificationHandler<ArticleCreatedEvent>
+{
+    private readonly ILogger<ArticleCreatedEventHandler> _logger;
+
+    public ArticleCreatedEventHandler(ILogger<ArticleCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(ArticleCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Domain Event: {DomainEvent}", notification.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Application/Articles/EventHandlers/ArticleDeletedEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Articles.EventHandlers;
+public class ArticleDeletedEventHandler : INotificationHandler<ArticleDeletedEvent>
+{
+    private readonly ILogger<ArticleDeletedEventHandler> _logger;
+
+    public ArticleDeletedEventHandler(ILogger<ArticleDeletedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(ArticleDeletedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Domain Event: {DomainEvent}", notification.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+}
