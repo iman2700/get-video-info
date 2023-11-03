f9532716e08b3d2a11c156ef02b89b380ae3e61e
alireza5a <saeedi.mail@gmail.com>
2023-11-03 15:30:09 +0330

    add Article Queries

---
+++ src/Application/Articles/Queries/GetArticlesWithPagination/ArticlePageDto.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Articles.Queries.GetArticlesWithPagination;
+public class ArticlePageDto
+{
+    public int Id { get; init; }
+    public string Title { get; init; } = String.Empty;
+    public string? Description { get; init; }
+    public string? Thumbnail { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Article, ArticlePageDto>();
+        }
+    }
+}
---
+++ src/Application/Articles/Queries/GetArticlesWithPagination/GetArticlesWithPagination.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using Application.Common.Interfaces;
+using Application.Common.Mappings;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+namespace Application.Articles.Queries.GetArticlesWithPagination;
+public record GetArticlesWithPaginationQuery : IRequest<PaginatedList<ArticlePageDto>>
+{
+    public int PageNumber { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
+
+public class GetTodoItemsWithPaginationQueryHandler : IRequestHandler<GetArticlesWithPaginationQuery, PaginatedList<ArticlePageDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetTodoItemsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<ArticlePageDto>> Handle(GetArticlesWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Articles
+            .OrderBy(x => x.Title)
+            .ProjectTo<ArticlePageDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+    }
+}
