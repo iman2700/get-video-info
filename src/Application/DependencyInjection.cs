868b813e267e1b1f41c8e81ed6806a99171bb6fd
alireza5a <saeedi.mail@gmail.com>
2023-11-05 23:54:31 +0330

    add UnhandledException Behaviour

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -13,6 +13,7 @@ public static class DependencyInjection
 
         services.AddMediatR(cfg => {
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<>));
         });
