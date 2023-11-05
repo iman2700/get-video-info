9015c87481209824854f648bd9aea7a6c529018f
alireza5a <saeedi.mail@gmail.com>
2023-11-05 23:44:29 +0330

    delete unused namespaces

--- src/Domain/Events/ArticleCreatedEvent.cs
+++ src/Domain/Events/ArticleCreatedEvent.cs
@@ -1,12 +1,4 @@
-﻿using Domain.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Entities;
-
-namespace Domain.Events;
+﻿namespace Domain.Events;
 public class ArticleCreatedEvent : BaseEvent
 {
     public ArticleCreatedEvent(Article item)
--- src/Domain/Events/ArticleDeletedEvent.cs
+++ src/Domain/Events/ArticleDeletedEvent.cs
@@ -1,12 +1,4 @@
-﻿using Domain.Common;
-using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Events;
+﻿namespace Domain.Events;
 public class ArticleDeletedEvent : BaseEvent
 {
     public ArticleDeletedEvent(Article item)
