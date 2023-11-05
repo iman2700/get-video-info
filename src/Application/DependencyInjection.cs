ac33c3fe95c37fb2f93b44be501bc7e8390cb20b
alireza5a <saeedi.mail@gmail.com>
2023-11-06 00:00:37 +0330

    add Performance Behaviour

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -17,6 +17,7 @@ public static class DependencyInjection
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
         });
 
         return services;
