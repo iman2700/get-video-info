3df47be931b4d30164d22f7dfdfb48a3329d84fb
alireza5a <saeedi.mail@gmail.com>
2023-11-07 19:42:50 +0330

    complete infrastructure dbcontext

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -13,11 +13,25 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.Extensions.Options;
+using MediatR;
+using Infrastructure.Data.Interceptors;
 
 namespace Infrastructure.Data;
 public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions) { }
+    private readonly IMediator _mediator;
+    private readonly AuditableEntityInterceptor _auditableEntitySaveChangesInterceptor;
+
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions,
+        IMediator mediator,
+        AuditableEntityInterceptor auditableEntitySaveChangesInterceptor)
+        : base(options, operationalStoreOptions)
+    {
+        _mediator = mediator;
+        _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+    }
 
     public DbSet<Article> Articles => Set<Article>();
 
@@ -26,5 +40,17 @@ public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, 
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
         base.OnModelCreating(builder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await _mediator.DispatchDomainEvents(this);
+
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
