b5259ba3de714b374f3bc2ddfa7822e3d52faaac
alireza5a <saeedi.mail@gmail.com>
2023-11-07 11:51:12 +0330

    Change mapping method

--- src/Application/Articles/Queries/GetArticlesWithPagination/ArticlePageDto.cs
+++ src/Application/Articles/Queries/GetArticlesWithPagination/ArticlePageDto.cs
@@ -1,18 +1,12 @@
-﻿using Domain.Entities;
+﻿using Application.Common.Mappings;
+using Domain.Entities;
 
 namespace Application.Articles.Queries.GetArticlesWithPagination;
-public class ArticlePageDto
+
+public class ArticlePageDto : IMapFrom<Article>
 {
     public int Id { get; init; }
     public string Title { get; init; } = String.Empty;
     public string? Description { get; init; }
     public string? Thumbnail { get; init; }
-
-    private class Mapping : Profile
-    {
-        public Mapping()
-        {
-            CreateMap<Article, ArticlePageDto>();
-        }
-    }
 }
