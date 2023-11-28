0bbb4e664ef50eaa15595d4da94993e0ac50cbf2
alireza5a <saeedi.mail@gmail.com>
2023-11-29 02:45:54 +0330

    add TagsController

---
+++ src/Application/Tags/Queries/GetTags.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Application.NewsItems.Queries.GetNewsItemsWithPagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.Tags.Queries;
+
+public record GetTagsQuery : IRequest<List<Tag>>;
+
+public class GetTagsQueryHandler : IRequestHandler<GetTagsQuery, List<Tag>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetTagsQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Tag>> Handle(GetTagsQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Tags.ToListAsync(cancellationToken: cancellationToken);
+    }
+}
---
+++ src/WebApi/Controllers/TagsController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Models;
+using Application.NewsItems.Queries.GetNewsItemsWithPagination;
+using Application.Tags.Queries;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+
+public class TagsController : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Tag>>> GetTags()
+    {
+        GetTagsQuery query = new GetTagsQuery();
+        return await Mediator.Send(query);
+    }
+}
