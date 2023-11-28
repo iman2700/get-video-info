e08ea3889dbdf24324839d3df04fda9ac23dca44
alireza5a <saeedi.mail@gmail.com>
2023-11-28 13:08:03 +0330

    add Login to Dashboard

--- src/Dashboard/Program.cs
+++ src/Dashboard/Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Dashboard.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient("LocalApi", client => client.BaseAddress = new Uri("https://localhost:81/"));
+builder.Services.AddScoped<ILocalStorageService, LocalStorageService>();
 
 var app = builder.Build();
 
